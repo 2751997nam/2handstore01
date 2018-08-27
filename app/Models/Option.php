1170083db8c7f2867cd869b52285b31134793d11
2751997nam <nambcvt275@gmail.com>
2018-08-28 00:08:22 +0700

    public storage

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Option extends Model
 {
@@ -21,7 +22,7 @@ class Option extends Model
         if (count($options) > 0) {
             foreach ($options as $option) {
                 if (in_array($option->key, config('site.assets'))) {
-                    $data[$option->key] = asset('storage/site/' . $option->value);
+                    $data[$option->key] = asset(Storage::url(config('site.folder') . '/' . $option->value));
                 } else {
                     $data[$option->key] = $option->value;
                 }
